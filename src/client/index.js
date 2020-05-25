9d355869ac73a566130e8f41abed970a93d635a6
Adri Forczek <forczekadri@gmail.com>
2020-05-25 23:45:14 +0100

    chore: adds logo

--- src/client/index.js
+++ src/client/index.js
@@ -6,10 +6,15 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
+// adds main image to screen
 import mainImage from './images/nlp.jpg'
-
 const imageContainer = document.getElementById('main_image');
 imageContainer.src = mainImage;
+
+// adds logo to screen
+import logo from './images/logo.png'
+const logoContainer = document.getElementById('logo');
+logoContainer.src = logo;
 
 // Js files are being exported to 'Client' library (Output set in webpack.dev.js)
 export {
